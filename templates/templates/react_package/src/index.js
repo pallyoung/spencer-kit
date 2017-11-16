2a78008cd9bbf9d5d1e147980ea42b1c7e764c17
Spencer <yeshaopeng@lianqin360.com>
2017-11-16 14:19:02 +0800

    react-package

--- templates/templates/react_package/src/index.js
+++ templates/templates/react_package/src/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 module.exports = function(){
     return <div>welcome to use spencer-kit-cli(https://github.com/pallyoung/spencer-kit) to init project</div>
 }
--- templates/templates/react_package/webpack.config.js
+++ templates/templates/react_package/webpack.config.js
@@ -1,11 +1,12 @@
 var path = require('path');
 var webpack = require('webpack');
 module.exports = {
-    context: path.resolve(__dirname, './'),
-    entry:path.resolve('./index.js'),
+    context: path.resolve('./'),
+    entry:path.resolve('./demo/index.js'),
     output:{
         path:path.resolve('./demo'),
-        filename:'index.bundle.js'
+        filename:'index.bundle.js',
+        publicPath: ""
     },
     module: {
         rules: [
@@ -21,7 +22,7 @@ module.exports = {
         new webpack.HotModuleReplacementPlugin()
     ],
     devServer: {
-        contentBase:path.resolve('./'),
+        contentBase:path.resolve('./demo'),
         compress: true,
         port: 8000,
         hot: true
